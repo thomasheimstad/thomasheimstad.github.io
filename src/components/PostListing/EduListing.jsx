334a733999a04e58a737766e2304a5bf1a157f1f
thomasheimstad <thomasheimstad@gmail.com>
2020-10-15 18:38:24 +0200

    removed punctuation from eduListing

--- src/components/PostListing/EduListing.jsx
+++ src/components/PostListing/EduListing.jsx
@@ -10,8 +10,8 @@ export default function EduListing(){
           <h3>Master of Music</h3>
           <p>2013-2015</p>
           <p>Koninklijk Conservatorium Den Haag, The Netherlands</p>
-          <p>Classical guitar w/ Prof. Zoran Dukić.</p>
-          <p>Graduated cum laude.</p>
+          <p>Classical guitar w/ Prof. Zoran Dukić</p>
+          <p>Graduated cum laude</p>
         </div>
         <div>
           <h3>60 ECTS Psychology</h3>
@@ -23,13 +23,13 @@ export default function EduListing(){
           <h3>Erasmus exchange</h3>
           <p>2011-2012</p>
           <p>Musikene - Centro Superior de Música del País Vasco, Spain</p>
-          <p>Classical guitar w/ Prof. Marco Socías.</p>
+          <p>Classical guitar w/ Prof. Marco Socías</p>
         </div>
         <div>
           <h3>Bachelor classical music</h3>
           <p>2008-2012</p>
           <p>University of Bergen – The Grieg Academy, Norway</p>
-          <p>Classical guitar w/ Prof. Stein-Erik Olsen.</p>
+          <p>Classical guitar w/ Prof. Stein-Erik Olsen</p>
         </div>
       </div>
     </div>
